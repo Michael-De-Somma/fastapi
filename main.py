4990513cec8b808f512b814e61dd7a7af4f386b9
Michael-De-Somma <124059694+Michael-De-Somma@users.noreply.github.com>
2023-05-12 18:28:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def home():
-    return 'Time to get some encounter count!!!..... I Hope.....'
+    return 'Member_month'
 
 @app.route('/preview', methods=["GET"])
 def preview():
